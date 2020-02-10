1e7adb305f02687d6ec4d687021edfa6f043f482
qyang <qyang1219@gmail.com>
2020-02-10 11:48:08 +0800

    [nest] set body limit

--- nest/src/main.ts
+++ nest/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'data/images'), {
     prefix: '/images/',
   });
+  app.use(bodyParser.json({limit: '50mb'}));
+  app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
   app.enableCors();
   await app.listen(8080);
 }
